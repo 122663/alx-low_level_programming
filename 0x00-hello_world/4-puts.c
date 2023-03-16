e7b6570ca638c02b5d39c2e2162dfa9c93dca102
ikii <ikbal.bouadel@gmail.com>
2023-03-16 15:48:14 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * this a comment for main and return func
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0) ;
+}
