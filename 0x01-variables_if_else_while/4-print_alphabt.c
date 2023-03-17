cf77296109cad0f97068718d3a0eea7df838f502
ikii <ikbal.bouadel@gmail.com>
2023-03-17 16:41:11 -0700

    if et for)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+for (i = 97; i < 123; ++i)
+{
+if (i == 101 || i == 113)
+{ ++i;
+continue;
+}
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
