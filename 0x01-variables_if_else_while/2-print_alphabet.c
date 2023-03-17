a1979a70953f8f14608aadb957fb77a32d1e6495
ikii <ikbal.bouadel@gmail.com>
2023-03-17 15:49:48 -0700

    for

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+	for (i = 97; i < 123; ++i)
+	{ putchar(i);
+	putchar('\n'); }
+	return (0);
+}
