b908f5b5f4a33d428d7680e15521c53344c45631
ikii <ikbal.bouadel@gmail.com>
2023-03-16 17:27:24 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,5 +4,5 @@
  * Return: final to exit
  */
 int main(void)
-{ fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
+{ fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n, stdout");
 	return (1); }
