7824f04c2de2dd4143acdcfac1b30186fffdfdfa
ikii <ikbal.bouadel@gmail.com>
2023-03-17 16:31:49 -0700

    double for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+int j;
+for (i = 97; i < 123; ++i)
+{ putchar(i); }
+for (j = 65; j < 91; ++j)
+{ putchar(j); }
+putchar('\n');
+return (0);
+}
