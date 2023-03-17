ca4c3e422a30e0330384433241f900bda5552d0e
ikii <ikbal.bouadel@gmail.com>
2023-03-17 16:54:50 -0700

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
