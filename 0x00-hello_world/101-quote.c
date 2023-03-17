1bd699a5484ce4639076c47bd0cf9fbd7f078119
ikii <ikbal.bouadel@gmail.com>
2023-03-16 17:48:08 -0700

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,5 +4,6 @@
  * Return: final to exit
  */
 int main(void)
-{ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+{	
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1); }
