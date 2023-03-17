57526d2bf44371b83159c8e44f7b9c5f983ba083
ikii <ikbal.bouadel@gmail.com>
2023-03-17 16:50:10 -0700

    for plus if

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,15 @@
  */
 int main(void)
 { int i;
-for (i = 97; i <= 122; i++)
+i = 97;
+while (i <= 122)
 {
 if (i == 101 || i == 113)
 { i++;
 continue;
 }
 putchar(i);
+i++;
 }
 putchar('\n');
 return (0);
