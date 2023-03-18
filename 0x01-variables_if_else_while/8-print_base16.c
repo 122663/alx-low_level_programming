18d9428f7f733cd05b93079a42923883390ecaaf
ikii <ikbal.bouadel@gmail.com>
2023-03-17 17:18:06 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+int j;
+for (i = 48; i <= 57; i++)
+{ putchar(i); }
+for (j = 97; j <= 102; j++)
+{ putchar(j);
+}
+putchar('\n');
+return (0);
+}
