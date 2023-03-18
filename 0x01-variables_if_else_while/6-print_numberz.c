4a957608f7e5a36643f945652bbecd713687379b
ikii <ikbal.bouadel@gmail.com>
2023-03-17 17:02:56 -0700

    num++

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the key of the program
+ * Return: final to exit
+ */
+int main(void)
+{ int i;
+for (i = 0; i <= 9; ++i)
+putchar(i + 48);
+putchar('\n');
+return (0);
+}
