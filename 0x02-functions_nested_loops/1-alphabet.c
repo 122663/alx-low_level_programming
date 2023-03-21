88e9411cfdb263f4414482f383c7ca66b25b87d8
ikii <ikbal.bouadel@gmail.com>
2023-03-21 15:43:29 -0700

    _putchar alphab

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{ int i;
+i = 97;
+while (i < 123)
+{ _putchar(i);
+i++;
+_putchar('\n');
+}
+}
