055373cb649b6c96fb1a9c8936a3385cfb166045
ikii <ikbal.bouadel@gmail.com>
2023-03-21 15:52:55 -0700

    10xalphab

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,14 @@
  * print_alphabet - Make the alphabet
  * Return: void
  */
-void print_alphabet(void)
-{ int i;
+void print_alphabet_x10(void)
+{ int i, n;
+for (n = 0; n < 10; n++)
+{
 i = 97;
 while (i < 123)
 { _putchar(i);
 i++; }
 _putchar('\n');
+}
 }
