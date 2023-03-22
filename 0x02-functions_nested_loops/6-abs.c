450362251bebf6aa05639732406378ba227ec828
ikii <ikbal.bouadel@gmail.com>
2023-03-21 17:11:06 -0700

    valeur absolue

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Make the alphabet
+ * @i: chracter to traite
+ * Return: void
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+int n;
+n = i * -1;
+return (n);
+}
+return (i);
+}
