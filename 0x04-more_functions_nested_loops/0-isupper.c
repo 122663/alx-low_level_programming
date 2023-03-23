a024dbe28fec9c3e638fdc28a71d896700bad8e3
ikii <ikbal.bouadel@gmail.com>
2023-03-23 08:31:04 -0700

    uppecase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code.
+ * @c: charactere
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c < 91)
+{ return (1); }
+else
+{ return (0); }
+}
