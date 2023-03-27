38774006103b985ca9e548fc52f11e592c129db0
ikii <ikbal.bouadel@gmail.com>
2023-03-27 09:23:12 -0700

    switch variabme

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the charc a et b
+ * @a: first char
+ * @b: second char
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
